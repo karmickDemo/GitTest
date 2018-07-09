529af43c3f3a2145a7ede9f915668cecd76cfd28
Karmicksol218 <karmicksol218@gmail.com>
2018-07-09 15:14:03 +0530

    button action message changed

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func buttonClicked(_ sender: UIButton) {
-        print("This is a Git tutorial")
+        print("finished")
     }
 
     override func didReceiveMemoryWarning() {
