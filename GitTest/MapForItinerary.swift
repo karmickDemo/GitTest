9925917633b88fdb8cd7bcdae0a7e98e28f97384
Karmicksol218 <karmicksol218@gmail.com>
2018-07-09 13:15:13 +0530

    create a class for map feature

---
+++ GitTest/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitTest
+//
+//  Created by Karmick on 09/07/18.
+//  Copyright © 2018 karmick. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
